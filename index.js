f516fa1c2349e45b04df2150316232cbe477840b
Sky Randy <soras1758@gmail.com>
2024-06-22 18:17:27 +0900

    Add button to resize the sketcking grid.
    Made the whole process of making the 16x16 grid to a function so that user can change the grid size if needed.
    Made a function to check if the number entered isnt too big or small insize the re-sizing button function.

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const divContainer=document.querySelector('.divContainer');
 
-for(let i=0; i<=15;i++){
+let gridPerSide;
+
+function formatGrid(gridPerSide){
+    for(let i=0; i<=gridPerSide-1;i++){
     const divParent=document.createElement('div');
     divParent.classList.add(`divParent`);
     divContainer.appendChild(divParent);
-        for(let i=0;i<=15;i++){
+        for(let i=0;i<=gridPerSide-1;i++){
             const divChild=document.createElement('div');
             divChild.classList.add(`divChild`);
             divParent.appendChild(divChild);
 
             divChild.addEventListener('mouseover',(e)=>{
                 e.target.style.backgroundColor='#fcb103';
-            divChild.addEventListener('mouseout',(e)=>{
-                e.target.style.backgroundColor='';
+            divChild.addEventListener('museout',(e)=>{
             });
             });
     }
-}
+}}
 
+formatGrid(16);
+
+const genGridBtn=document.querySelector('button');
+genGridBtn.addEventListener('click',()=>{
+    gridPerSide=prompt('Input Grid Per Side Here!',16);
+    if(gridPerSide>100){
+        alert('Opps...Looks like the number that you entered was too big.');
+        genGridBtn.dispatchEvent(validNumberRequired);
+        return  
+    }else if(gridPerSide<1){
+        alert('Please enter valid number.')
+        genGridBtn.dispatchEvent(validNumberRequired);
+        return
+    }
+    divContainer.innerHTML='';
+    //delete exising grid before formatting new one.
+    formatGrid(gridPerSide);
+})
+
+//Alert until input number is valid
+let validNumberRequired= new Event('click');
